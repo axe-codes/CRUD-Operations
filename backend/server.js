3f0b0ae09413e749cd6fb055174f29bca2d9ee79
axe-codes <dipesh.sedani@gmail.com>
2024-03-22 12:42:54 +0530

    Deployment 01

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -10,18 +11,21 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); //to parse the incoming requests with JSON payloads (from req.body)
 
 app.use("/api", userRoutes);
 
-// app.get("/", (req, res) => {
-//   root route "http://localhost:5000/"
-//   res.send("Hello world");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   connectToMongoDB();
-  console.log(`server is running on port ${PORT} `);
+  console.log(`server is running on port ${PORT}`);
 });
